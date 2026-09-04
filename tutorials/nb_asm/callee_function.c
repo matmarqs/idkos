f155b43b29bc3e7cbda9c5e09c49e07556af48ed
Mateus Marques <marqs.mts@gmail.com>
2026-09-04 09:59:52 -0300

    It's better to just move on. Git checkout if need be. The TAGS and LSP were confused with the old code.

--- tutorials/nb_asm/callee_function.c
+++
@@ -1,5 +0,0 @@
-int callee_function(int);
-
-void caller_function() { callee_function(0xdede); }
-
-int callee_function(int my_arg) { return my_arg; }
--- tutorials/osdev_grub_uefi/kernel/include/kernel/io.h
+++
@@ -1,9 +0,0 @@
-#ifndef _KERNEL_IO_H
-#define _KERNEL_IO_H
-
-#include <stddef.h>
-
-int get_cpu_vendor_string(char vendor_string[13]);
-int poll_keyboard(void);
-
-#endif /* _KERNEL_IO_H */
